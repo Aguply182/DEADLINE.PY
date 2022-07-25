e1370141a375f85aca3d416dd23a5d23a02dbae8
Aguply182 <101477884+Aguply182@users.noreply.github.com>
2022-07-25 13:50:44 +0800

    Add files via upload

---
+++ telebot/__init__.py
@@ -0,0 +1,22 @@
+#  DeadLineTeam - The example use of Pyrogram library and for learning
+#  Copyright (C) 2022 DeadLineTeam <https://github.com/vickysaputraa>
+#
+#  This file is part of DeadLine.
+#
+#  DeadLineTeam is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  DeadLineTeam is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with DeadLineTeam.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__    = "Vicky Saputra A.K.A Shizuka-Chan"
+__copyright__ = "Copyright (C) DeadLineTeam <https://github.com/vickysaputraa>"
+__license__   = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__version__   = "1.0"
